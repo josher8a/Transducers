c49cfc54ec411e56fe23d362fe0014ba907a54fe
Joshua <josher8a@gmail.com>
2022-01-05 14:52:11 -0400

    reduce based map

--- stepByStep.js
+++ stepByStep.js
@@ -3,14 +3,25 @@ const dragons = [
     { name: 'wargle', size: 3 }
 ]
 
-let title= dragons.map(dragon => {
+let dragonTitle = dragon => {
     if (dragon.size > 1) {
         return dragon.name + ' the great'
     }
     return dragon.name + ' the tiny'
-})
+}
+
+
+let title= dragons.map(dragonTitle) 
 
 title
 
 // reimplemented map function with reduce 
 
+function map(array, callback) {
+    return array.reduce((acc, current) => {
+        acc.push(callback(current))
+        return acc
+    }, [])
+}
+
+title = map(dragons, dragonTitle)
